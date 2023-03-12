4a6574083b9e00a654e364f03cee34107c901b3a
Abid Hasan <ab1dh4san@gmail.com>
2023-03-12 22:15:11 +0600

    mongoose added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+require('dotenv').config()
 
-app.get('/', (req, res) => {
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (_, res) => {
 	res.send('Up and running...')
+})
+
+app.post('/new-user', (req, res) => {
+	console.log(req.body)
 })
 
 app.listen(process.env.PORT || 5000, () => console.log(`Running on port ${process.env.PORT || 5000}`))
